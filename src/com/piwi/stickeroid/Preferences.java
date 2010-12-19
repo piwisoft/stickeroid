624d9b8bec34601c3289a9db4de8aa31b46aec4d
pascal.wils@gmail.com <pascal.wils@gmail.com@aebb5163-083e-c537-1fa4-c9728d95cb22>
2010-12-19 22:18:36 +0000

    Version 1.0

---
+++ src/com/piwi/stickeroid/Preferences.java
@@ -0,0 +1,17 @@
+package com.piwi.stickeroid;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setTitle(R.string.preferences_title);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
